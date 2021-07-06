756a2d6c76e75b09699c175a937b45242400fc73
bill <wcstrickland1@gmail.com>
2021-07-05 23:49:20 -0500

    updated scaper to fetch query string instead of inputing the search

---
+++ dependencies/tar_help.md
@@ -0,0 +1,8 @@
+# extracting the tar file
+
+
+```
+
+sudo tar -xzfv geckodriver-v0.29.1-linux32.tar.gz
+
+```
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.firefox.options import Options
 
-#   command line arguments
-args = sys.argv[1:]
+    
+
 
 #   webdriver init
 
@@ -20,27 +20,39 @@ args = sys.argv[1:]
 
 driver = webdriver.Firefox()
 
-#   traversal
-results = {}
+#   dealing with cookie agreements
 driver.get('https://www.lego.com/en-us/page/static/pick-a-brick')
 continue_button = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_css_selector('button.Button__Base-sc-1jdmsyi-0:nth-child(4)') )
 continue_button.click()
 accept_all_button = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_css_selector('button.aKFCv:nth-child(2)'))
 accept_all_button.click()
 
+#   init an empty dict for storage
+results = {}
+
+#TODO get items from db instead of cmd line
+#   iterate over items
+args = sys.argv[1:] # cmd line args
 for item in args:
-    search_field = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_css_selector('#pab-search'))
-    search_apply = WebDriverWait(driver, timeout=20).until(lambda d: d.find_element_by_css_selector('.iEXjfw'))
-    search_field.clear()
-    search_field.send_keys(item)
-    search_apply.click()
+    if "/" not in item:
+        print(f"{item}: is not a valid lego item number format")
+        continue
+    item_split = item.split(sep="/")
+    driver.get(f'https://www.lego.com/en-us/page/static/pick-a-brick?query={item_split[0]}%2F{item_split[1]}&page=1')
     try:
         time.sleep(.1)
         driver.find_element_by_css_selector('li.ElementsListstyles__Leaf-d5a7o-1:nth-child(1) > div:nth-child(1) > div:nth-child(4) > div:nth-child(1) > button:nth-child(1)')
-        results[item] = "in stock"
+        results[item] = "1"
     except:
-        results[item] = "out of stock"
+        results[item] = "0"
 
 driver.quit()
+
+print("#"*40)
 for k, v in results.items():
-    print(f"{k} : {v}")
+    if v == "1":
+        print(f"{k} : in stock!")
+    else:
+        print(f"{k} : out of stock :(")
+
+#TODO insert into a db instead of stdout
