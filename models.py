a8808f5e7741cf11b3ec0a023475e8db9f364abe
bill <wcstrickland1@gmail.com>
2021-07-07 15:35:17 -0500

    object insert methods working

--- models.py
+++ models.py
@@ -20,14 +20,14 @@ class User:
         self.items = (self.i1, self.i2, self.i3)
 
     def insert(self, cur, conn):
-        stmt = f"INSERT INTO users(id, uname, item1, item2, item3) VALUES(%s,%s,%s,%s,%s)"
+        stmt = f"INSERT INTO users(uid, uname, item1, item2, item3) VALUES(%s,%s,%s,%s,%s)"
         vals = (self.uid, self.uname, *self.items)
         try:
             cur.execute(stmt, vals)
             conn.commit()
             print("Insert successful!")
         except (Exception, psycopg2.DatabaseError) as error:
-            print("Error: ", error)
+            print("User.insert Error: ", error)
 
 
 class StockReport:
@@ -38,24 +38,24 @@ class StockReport:
         self.i1 = i1
         self.i2 = i2
         self.i3 = i3
-        self.items = (i1, i2, i3)
+        self.items = (self.i1, self.i2, self.i3)
 
     def update(self, i1,i2,i3):
         self.i1 = i1
         self.i2 = i2
         self.i3 = i3
-        self.items = (i1, i2, i3)
+        self.items = (self.i1, self.i2, self.i3)
 
     def insert(self, cur, conn):
-        drop_statement = f"drop table {self.uname}"
+        drop_statement = f"drop table if exists {self.uname}"
         table_statement = f"create table {self.uname}(item VARCHAR(255), status VARCHAR(255))"    
-        insert_statement = f"insert into {name} (item, status) VALUES (%s, %s)"
+        insert_statement = f"insert into {self.uname} (item, status) VALUES (%s, %s)"
         try:
             cur.execute(drop_statement)
             cur.execute(table_statement)
-            for item in StockReprot.items:
-                cur.execute(insert_statement, item["item"], item["status"])
+            for item in self.items:
+                cur.execute(insert_statement, (item["item"], item["status"]))
             conn.commit()
             print("Insert Successufl!")
         except (Exception, psycopg2.DatabaseError) as error:
-            print("Error: ", error)
+            print("StockReport.insert Error: ", error)
--- seed.py
+++ seed.py
@@ -5,12 +5,36 @@ import models
 
 conn, cur = db.db_connect()
 
+drop_stmt = "drop table users"
+create_stmt = "create table users(uid varchar(255),uname varchar(255),item1 varchar(255),item2 varchar(255),item3 varchar(255))"
+
+cur.execute(drop_stmt)
+print("table dropped")
+cur.execute(create_stmt)
+print("table created")
+
 u1 = models.User(str(uuid.uuid4()), "bill", "888/999", "333/222", "444/666")
 u1.insert(cur, conn)
 
 u2 = models.User(str(uuid.uuid4()), "connor", "333/333", "999/999", "121/121")
 u2.insert(cur, conn)
 
+si1 = {
+    "item": "888/999",
+    "status": "true"
+}
+si2 = {
+    "item": "333/222",
+    "status": "true"
+}
+si3 = {
+    "item": "444/666",
+    "status": "false"
+}
+results = [si1, si2, si3]
+
+stock = models.StockReport(u1.uid, u1.uname, *results)
+stock.insert(cur, conn)
 
 cur.close()
 conn.close()
